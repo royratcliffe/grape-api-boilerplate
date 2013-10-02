29b3f066c24fc7dccbc23a5fe6132759adf67206
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-10-02 12:02:07 +0100

    Automatically mount APIs

--- app/interfaces/api.rb
+++ app/interfaces/api.rb
@@ -5,5 +5,16 @@ class API < Grape::API
   content_type :hal, 'application/hal+json'
   formatter :hal, Grape::Formatter::SerializableHash
 
-  mount Ping::API => '/ping'
+  # Automatically mount APIs. No need to know what APIs exist in advance. The
+  # super-API supports many, dynamically. The APIs can be Git-hosted sub-modules
+  # cloned under the app/interfaces directory. Look for api.rb files in all the
+  # app/interfaces subfolders. Mount the API at a mount point corresponding to
+  # its sub-directory path.
+  ActiveSupport::Dependencies.autoload_paths.each do |interfaces_dir|
+    Dir[File.join(interfaces_dir, '*', 'api.rb')].each do |api_rb|
+      relative_api_rb = Pathname.new(api_rb).relative_path_from(Pathname.new(interfaces_dir)).to_path
+      path = relative_api_rb.split(File::SEPARATOR)[0...-1].join(File::SEPARATOR)
+      mount "#{path}::API".camelize.constantize => File.join(File::SEPARATOR, path)
+    end
+  end
 end
